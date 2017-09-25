6eaa7775a733f65528605479a4c5519777e3c52e
Jean-Denis Boivin <jeandenis.boivin@gmail.com>
2017-09-25 08:41:55 -0400

    Renamed func in OptionTools

--- src/OptionTools.hx
+++ src/OptionTools.hx
@@ -6,14 +6,14 @@ using OptionTools;
 
 class OptionTools {
     
-    public static inline function mapFromNullable<T,U>( obj : T, f : T->U ) : Option<U> {
-        return switch( obj.fromNullable() ) {
-            case Some(obj) : f(obj).fromNullable();
+    public static inline function mapOption<T,U>( obj : T, f : T->U ) : Option<U> {
+        return switch( obj.option() ) {
+            case Some(obj) : f(obj).option();
             case None : None;
         }
     }
 
-    public static inline function fromNullable<T>( obj : T ) : Option<T> {
+    public static inline function option<T>( obj : T ) : Option<T> {
         return obj == null ? None : Some(obj);
     }
 }
