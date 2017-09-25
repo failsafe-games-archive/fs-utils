477659347a2abc0c9945cecf0fd644020f6ed8e2
Jean-Denis Boivin <jeandenis.boivin@gmail.com>
2017-09-24 21:10:29 -0400

    Add some Math utils

--- src/OptionTools.hx
+++ src/OptionTools.hx
@@ -2,8 +2,17 @@ package;
 
 import haxe.ds.Option;
 
+using OptionTools;
+
 class OptionTools {
     
+    public static function mapFromNullable<T,U>( obj : T, f : T->U ):Option<U> {
+        return switch(obj.fromNullable()) {
+            case Some(obj) : f(obj).fromNullable();
+            case None : None;
+        }
+    }
+
     public static function fromNullable<T>( obj : T ):Option<T> {
         return obj == null ? None : Some(obj);
     }
